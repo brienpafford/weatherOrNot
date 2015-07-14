c22485ada3fbb267b499aa2242d842923e650ec3
Brien Pafford <bcpaff@gmail.com>
2015-07-14 14:49:51 -0500

    moved search controller to templates, added weather view and controller

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'won.search'])
+angular.module('starter', ['ionic', 'starter.controllers', 'won.search', 'won.weather'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -34,8 +34,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'won.search'])
     url: "/search",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html",
+        templateUrl: "templates/search/search.html",
         controller: 'SearchCtrl'
+      }
+    }
+  })
+
+  .state('app.weather', {
+    url: "/weather",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/weather/weather.html",
+        controller: 'WeatherCtrl'
       }
     }
   })
