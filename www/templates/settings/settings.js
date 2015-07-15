265f5f04f6f1723b2782c3b2d8a3e7b504655f7f
Brien Pafford <bcpaff@gmail.com>
2015-07-15 11:45:15 -0500

    Added settings for degree display, $scope responding to html

--- www/templates/settings/settings.js
+++ www/templates/settings/settings.js
@@ -1,4 +1,23 @@
 angular.module('won.settings', [])
 
-.controller('SettingsCtrl', function() {
+.controller('SettingsCtrl', function(settings, $scope, $ionicLoading) {
+
+  $scope.precision = settings.scale;
+  $scope.scale = settings.precision;
+
+$scope.randomScale = function () {
+  $ionicLoading.show({
+    //template: '<img src=''>',
+    duration: 1000
+  });
+}
+
+})
+
+
+.factory('settings', function () {
+  return {
+    scale: 'F',
+    precision: 1
+  };
 });
